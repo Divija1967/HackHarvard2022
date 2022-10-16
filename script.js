baa1f87ebf62279900e8a52faed9b3df7e2d7626
Dij1022 <90654140+Divija1967@users.noreply.github.com>
2022-10-16 02:50:39 -0400

    Basic opening, search results, and recipe pages

---
+++ script.js
@@ -0,0 +1,29 @@
+
+// timer function
+// const timeS = document.getElementById('timer');
+// let timeSeconds = 45;
+
+// timeS.innerHTML = `00:${timeSeconds}`;
+
+// const countDown  = setInterval(()=>{
+//     timeSeconds--;
+//     displayTime(timeSeconds)
+//     if(timeSeconds <= 0 || timeSeconds < 1){
+//         clearInterval(countDown)
+        
+//     }
+// }, 1000);
+
+// function displayTime(second){
+//     const min = Math.floor(second / 60);
+//     const sec = Math.floor(second % 60);
+//     timeS.innerHTML = `${min}:${sec}`;
+// }
+
+// on clicking the search button, open new page
+function search(){
+    
+}
+
+document.getElementsByClassName("input-bar-lock").disabled = true;
+
